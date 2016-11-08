131c4c174f4c41932aa9cb24d087f37892091e11
Guillaume Petitpierre <coomania@gmail.com>
2016-11-08 11:20:34 +0100

    Display activity now fullscreen

--- Swatching/app/src/main/java/io/infinit8/swatching/DisplayMovie.java
+++ Swatching/app/src/main/java/io/infinit8/swatching/DisplayMovie.java
@@ -1,12 +1,20 @@
 package io.infinit8.swatching;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -19,7 +27,29 @@ public class DisplayMovie extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_display_movie);
 
+        FrameLayout fl = (FrameLayout)findViewById(R.id.imgLayout);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getRealSize(size);
+
+        TypedValue tv = new TypedValue();
+        getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true);
+        int actionBarHeight = getResources().getDimensionPixelSize(tv.resourceId);
+        fl.getLayoutParams().height = size.y-getNavigationBarHeight()-actionBarHeight;
+        fl.requestLayout();
 
 
+    }
+
+    public int getNavigationBarHeight()
+    {
+        boolean hasMenuKey = ViewConfiguration.get(this).hasPermanentMenuKey();
+        int resourceId = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0 && !hasMenuKey)
+        {
+            return getResources().getDimensionPixelSize(resourceId);
+        }
+        return 0;
     }
 }
--- Swatching/app/src/main/java/io/infinit8/swatching/ScrollingActivity.java
+++ Swatching/app/src/main/java/io/infinit8/swatching/ScrollingActivity.java
@@ -1,11 +1,15 @@
 package io.infinit8.swatching;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -13,6 +17,5 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-
     }
 }
