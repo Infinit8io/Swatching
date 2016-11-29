ba01eb15e1f8dc9bf091e939fab343d9043cdcf4
Guillaume Petitpierre <guillaume.petitpierre@bluewin.ch>
2016-11-29 11:03:46 +0100

    Removed API key, thanks Dom

--- Swatching/app/src/main/java/io/infinit8/swatching/Config.java
+++
@@ -1,9 +0,0 @@
-package io.infinit8.swatching;
-
-/**
- * Created by Dom on 08.11.2016.
- */
-
-public class Config {
-    public static final String TMDB_API_KEY = "a49056f6ecd497271a1d6834096d4ab2";
-}
