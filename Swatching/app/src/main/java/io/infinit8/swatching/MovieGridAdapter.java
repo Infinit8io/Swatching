e656b7286b1de7e30728f7d1845aa70dd07a2d4e
dominiqu.ducommun <dominique.ducommun@he-arc.ch>
2016-11-29 10:07:08 +0100

    Cool grid layout for movie choice

---
+++ Swatching/app/src/main/java/io/infinit8/swatching/MovieGridAdapter.java
@@ -0,0 +1,59 @@
+package io.infinit8.swatching;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Dom on 29.11.2016.
+ */
+
+
+public class MovieGridAdapter extends ArrayAdapter<Movie> {
+    private Context context;
+    private int layoutResourceId;
+    private ArrayList<Movie> data = new ArrayList<Movie>();
+
+    public MovieGridAdapter(Context context, int layoutResourceId, ArrayList data) {
+        super(context, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        ViewHolder holder = null;
+
+        if (row == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+            holder = new ViewHolder();
+            holder.imageTitle = (TextView) row.findViewById(R.id.text);
+            holder.image = (ImageView) row.findViewById(R.id.image);
+            row.setTag(holder);
+        } else {
+            holder = (ViewHolder) row.getTag();
+        }
+
+        Movie item = data.get(position);
+
+        // Set movie image item infos
+        holder.imageTitle.setText(item.getTitle());
+        holder.image.setImageBitmap(item.getPoster());
+        return row;
+    }
+
+    static class ViewHolder {
+        TextView imageTitle;
+        ImageView image;
+    }
+}
