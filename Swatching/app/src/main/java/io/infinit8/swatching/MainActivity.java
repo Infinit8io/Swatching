04854c8c0057debf155ae7899d461c8f4b5a7009
Guillaume Petitpierre <guillaume.petitpierre@bluewin.ch>
2017-01-24 10:14:48 +0100

    Bye kitten.

--- Swatching/app/src/main/java/io/infinit8/swatching/MainActivity.java
+++ Swatching/app/src/main/java/io/infinit8/swatching/MainActivity.java
@@ -79,17 +79,6 @@ public class MainActivity extends AppCompatActivity {
             Point size = new Point();
             display.getSize(size);
 
-            WebView wv = (WebView)findViewById(R.id.iv_background);
-
-            wv.loadDataWithBaseURL("file://android_res/drawable/", "<img src='file:///android_res/drawable/kitten.gif' style='min-height: 100%; min-width:100%; height:auto; width:auto; position:absolute; top:-100%; bottom:-100%; left:-100%; right:-100%; margin:auto; overflow:hidden;'/>", "text/html", "utf-8", null);
-            wv.setHorizontalScrollBarEnabled(false);
-            wv.setVerticalScrollBarEnabled(false);
-            wv.setOnTouchListener(new View.OnTouchListener() {
-                public boolean onTouch(View v, MotionEvent event) {
-                    return(event.getAction() == MotionEvent.ACTION_MOVE);
-                }
-            });
-
             Button btnStart = (Button)findViewById(R.id.btnStart);
 
             btnStart.setOnClickListener(new View.OnClickListener() {
